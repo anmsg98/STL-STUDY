6946ef099ccead8ff385a4481b38fb3000087a08
DESKTOP-MOCJD6J\User <wjstjd1004@naver.com>
2021-03-30 15:18:41 +0900

    5주 1일차 완료

--- 2021-1-STL 화56목56/소스.cpp
+++ 2021-1-STL 화56목56/소스.cpp
@@ -1,18 +1,23 @@
 //-----------------------------------------------------------------
 // 2021. 1학기 STL 화56 목56  - 3월 30일 화요일            (5주 1일)
 //
-// String.h를 다운받아 공통된 관찰을 해 본다.
+// - vector
 //-----------------------------------------------------------------
 #include <iostream>
-#include <array>
-#include <algorithm>
+#include <vector>
 #include "save.h"
 #include "String.h"
 using namespace std;
 
+// [문제] vector란 무엇입니까 - 동적(컴파일시에는 크기를 알 수 없는) 배열입니다.
+
 int main()
 {	
-	
+	vector<int> v{ 1,2,3 };
 
+	cout << "원소의 개수는 - " << v.size() << endl;
+	v.push_back(10);
+	cout << "원소의 개수는 - " << v.size() << endl;
+	
 	//save("소스.cpp");
 }
