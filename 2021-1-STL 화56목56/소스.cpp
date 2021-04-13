f83bae7c7a220cac9f62b6a9b5d231101bf4bf64
DESKTOP-MOCJD6J\User <wjstjd1004@naver.com>
2021-04-13 15:20:14 +0900

    7주 1일차 완료

--- 2021-1-STL 화56목56/소스.cpp
+++ 2021-1-STL 화56목56/소스.cpp
@@ -1,25 +1,29 @@
 //-----------------------------------------------------------------
-// 2021. 1학기 STL 화56 목56  - 4월 8일 목요일            (6주 2일)
+// 2021. 1학기 STL 화56 목56  - 4월 13일 화요일            (7주 1일)
 //
-// deque
-//
-// 중간시험 - 4월 22일 (목요일)
+// 1. sequence 컨테이너
+//	원소를 삭제/추가 하는 동작이 0(1)
+//	- list
+//	- forward_list
 //-----------------------------------------------------------------
+
 #include <iostream>
-#include <deque>
+#include <list>
 #include "save.h"
 #include "String.h"
 using namespace std;
 
+
 int main()
 {	
-	deque<String> d;
-
-	d.emplace(d.end(), "1");
-	d.emplace(d.end(), "2");
-	d.emplace(d.end(), "3");
-	for (const String& s : d)
+	list<String> c{ 1,3,5,7,9,2,4,6,8,10 };
+	c.sort([](const String& a, const String& b) {
+		return a.size() < b.size();
+		});
+	//c.reverse();	// 리스트의 순서를 반대로 바꿔줌
+	// 전체를 순회하며 출력하자
+	for (const String& s : c)
 		cout << s << endl;
-	
+
 	//save("소스.cpp");
 }
