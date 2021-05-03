391d468f029edbedcfcfcd906e7caf53412a2243
anmsg98 <wjstjd1004@naver.com>
2021-05-03 15:23:35 +0900

    과제 완료

--- STL 과제1/2017180037전성희.cpp
+++ STL 과제1/2017180037전성희.cpp
@@ -20,29 +20,25 @@ public:
 		p = new char[num] {};
 		in.read((char*)p, num);
 	}
-	Player()
-	{
+	Player() {
 
 	}
 	void read(istream& is)
 	{
 		is.read((char*)this, sizeof(Player));
 		p = new char[num] {};
-		//cout << name << endl;
-		//cout << score << endl;
-		//cout << id << endl;
-		//cout << num << endl;
-		//cout << p << endl;
 		is.read((char*)p, num);
-		//cout << p << endl;
-		//cout << "=======================" << endl;
+	}
+	void write(ostream& os) {
+		os.write((char*)this, sizeof(Player));
+		os.write((char*)p, num);
 	}
 };
 
 
 ostream& operator<<(ostream& os, const Player& player)
 {
-	os << "이름: " << player.name << ", 아이디: " << player.id << ", 점수: " << player.score << ", 자원수: " << player.num /*<< ", 시작번지: " << player.p*/;
+	os << "이름: " << player.name << ", 아이디: " << player.id << ", 점수: " << player.score << ", 자원수: " << player.num;
 	return os;
 }
 
@@ -51,13 +47,13 @@ array<Player, 1000000> a;
 int main()
 {
 	/*1번*/
-	auto b = chrono::steady_clock::now();	//스톱워치 시작
-	ifstream in{ "Player 100만개",ios::binary };
+	ifstream in{ "2021 STL 과제 파일",ios::binary };
 	
 	for (int i = 0; i < 1000000; ++i)
 		a[i].read(in);
-
-	cout << a[999999] << endl;
+	
+	cout << *(a.end()-1) << endl;
+	cout << endl;
 
 	/*2번*/
 	int aver = 0;
@@ -66,19 +62,22 @@ int main()
 	cout << "점수의 평균값: " << aver / 1000000.0 << endl << endl;
 	
 	/*3번*/
+	int cnt{ 0 };
 	ofstream out("바이트 수가 500개"s, ios::binary);
 	for (auto i : a) {
 		if (i.num == 500) {
-			out.write((char*)&i, sizeof(Player));
+			i.write(out);
+			cnt++;
 		}
 	}
-	cout << "바이트 수가 500개.txt 생성완료" << endl << endl;
+	cout << "메모리 바이트 수가 500인 " << cnt << "개의 객체를 찾음" << endl;
+	cout << "'바이트 수가 500개' 파일 생성완료" << endl << endl;
 	
 	/*4번*/
-	int input_id = 10;
+	int input_id{ 0 };
 	while (true) {
 		cout << "id값 입력 (종료: ctrl+z): ";
-		//cin >> input_id;
+		cin >> input_id;
 		if (cin.eof() == true)
 			break;
 		
@@ -112,12 +111,5 @@ int main()
 			}
 		}
 		cout << endl;
-		auto e = chrono::steady_clock::now();	// 스톱워치 끝
-		auto d = e - b;
-		cout << "경과시간(초) - " << chrono::duration<double>(d).count() << endl << endl;
-		break;
-	}
-	
-	
-	
+	}	
 }
