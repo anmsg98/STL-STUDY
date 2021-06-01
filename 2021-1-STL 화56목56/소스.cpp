317f51ebec16410178faa09e1800069b360c0ab0
DESKTOP-MOCJD6J\User <wjstjd1004@naver.com>
2021-06-01 15:18:36 +0900

    Merge branch 'main' of https://github.com/anmsg98/STL-STUDY
    # Conflicts:
    #	2021-1-STL 화56목56/2021 1학기 STL 화56 목56 강의저장.txt
    #	2021-1-STL 화56목56/Release/2021-1-S.b8dd5e89.tlog/2021-1-STL 화56목56.write.1u.tlog
    #	2021-1-STL 화56목56/Release/2021-1-S.b8dd5e89.tlog/CL.read.1.tlog
    #	2021-1-STL 화56목56/Release/2021-1-STL 화56목56.log
    #	2021-1-STL 화56목56/Release/vc142.pdb
    #	2021-1-STL 화56목56/소스.cpp
    #	Release/2021-1-STL 화56목56.iobj
    #	Release/2021-1-STL 화56목56.ipdb
    #	Release/2021-1-STL 화56목56.pdb

--- 2021-1-STL 화56목56/소스.cpp
+++ 2021-1-STL 화56목56/소스.cpp
@@ -1,5 +1,9 @@
 //-------------------------------------------------------------------------
+<<<<<<< HEAD
+// 2021. 1학기 STL 화56 목56  - 6월 1일 화요일            (14주 1일)
+=======
 // 2021. 1학기 STL 화56 목56  - 5월 25일 화요일            (13주 1일)
+>>>>>>> 5459dd79ae62a484d6f612060d862f993c0308e8
 //
 // 알고리즘 함수 살펴보기
 // 알고리즘 사용예제(anagram)
@@ -10,10 +14,43 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <fstream>
+#include <set>
 #include "save.h"
 #include "String.h"
 using namespace std;
 
+<<<<<<< HEAD
+struct PS : public pair<string, string>
+{
+	PS() = default;
+	PS(string s) : pair<string, string>(s, s) {
+		sort(first.begin(), first.end());
+	}
+};
+
+int main()
+{
+	ifstream in{ "단어들.txt" };
+	vector<PS> v{ istream_iterator<string>{in}, {} };
+	// PS first 기준으로 정렬한다.
+	sort(v.begin(), v.end(), [](const PS& a, const PS& b) {
+		return a.first < b.first;
+		});
+
+	for (int i = 210000; i < 210200; ++i)
+		cout << v[i].first << " - " << v[i].second << endl;
+	
+	// [문제] 이 사전의 모든 애너그램 쌍을 출력하라
+	while (true) {
+		// 나와 다음 것이 같은 지 찾는다.
+		// if ( 찾음 )
+		//	 찾은 자리를 저장하고
+		//	 다음 위치와 다른 값이 나오는 자리를 찾는다.
+		//	 if (end())
+	}
+	
+=======
 // partition
 
 int main()
@@ -28,5 +65,6 @@ int main()
 	for (int n : v)
 		cout << n << " ";
 	cout << endl;
+>>>>>>> 5459dd79ae62a484d6f612060d862f993c0308e8
 	//save("소스.cpp");
 }
